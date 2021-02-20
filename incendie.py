ffba8c97af1e6c9b889c99e1221c95a32649efc6
Maxime EBRAN <maxime.ebran@ens.uvsq.fr>
2021-02-20 16:37:48 +0100

    Linter fait + placement des bouttons sauvegarder et charger

--- incendie.py
+++ incendie.py
@@ -32,21 +32,28 @@ liste_parcelle_plaine = []
 ###############################
 # Fonctions
 
+
 def generation():
     """Genere un terrain aleatoirement"""
 
-    for l in range(LARGEUR // 25):
-        for h in range (HAUTEUR // 25):
-            etat = rd.randint(0,2)
+    for x in range(LARGEUR // 25):
+        for y in range(HAUTEUR // 25):
+            etat = rd.randint(0, 2)
             if etat == 0:
-                canvas.create_rectangle((l * 25, h * 25), (l * 25 + 25, h * 25 + 25), fill="blue")
-                liste_parcelle_eau.append([l, h])
+                canvas.create_rectangle((x * 25, y * 25),
+                                        (x * 25 + 25, y * 25 + 25),
+                                        fill="blue")
+                liste_parcelle_eau.append([x, y])
             if etat == 1:
-                canvas.create_rectangle((l * 25, h*25), l * 25 + 25, h*25+25, fill = "green")
-                liste_parcelle_foret.append([l, h])
+                canvas.create_rectangle((x * 25, y * 25),
+                                        (x * 25 + 25, y * 25 + 25),
+                                        fill="green")
+                liste_parcelle_foret.append([x, y])
             if etat == 2:
-                canvas.create_rectangle((l * 25, h * 25), (l + 25 * 25, h * 25 + 25), fill="yellow")
-                liste_parcelle_plaine.append([l, h])
+                canvas.create_rectangle((x * 25, y * 25),
+                                        (x + 25 * 25, y * 25 + 25),
+                                        fill="yellow")
+                liste_parcelle_plaine.append([x, y])
 
 
 ###############################
@@ -57,12 +64,22 @@ racine.config(bg="black")
 
 
 canvas = tk.Canvas(racine, height=HAUTEUR, width=LARGEUR, bg="black")
-boutton_generation = tk.Button(racine, text="Génération du terrain", font=("Helvatica", "20"), bg="black", fg="white", command=generation)
+
+boutton_generation = tk.Button(racine, text="Génération du terrain",
+                               font=("Helvatica", "20"), bg="black",
+                               fg="white", command=generation)
+
+boutton_sauver = tk.Button(racine, text="Sauvegarder",
+                           font=("Helvatica", "20"), bg="black", fg="white")
+
+boutton_charger = tk.Button(racine, text="Charger", font=("Helvatica", "20"),
+                            bg="black", fg="white")
+
 
 canvas.grid(column=0, rowspan=3)
 boutton_generation.grid(row=0, column=1)
-
-
+boutton_sauver.grid(row=1, column=1)
+boutton_charger.grid(row=2, column=1)
 
 
 racine.mainloop()
