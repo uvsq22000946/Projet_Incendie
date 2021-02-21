73e55feeda06a93a815f5c62c0bd755ef7047aef
Maxime EBRAN <maxime.ebran@ens.uvsq.fr>
2021-02-21 16:36:36 +0100

    Mise en place de la liste des états

--- incendie.py
+++ incendie.py
@@ -25,9 +25,7 @@ LARGEUR = 600
 ###############################
 # Variables globales
 
-liste_parcelle_eau = []
-liste_parcelle_foret = []
-liste_parcelle_plaine = []
+liste_parcelle = []
 
 ###############################
 # Fonctions
@@ -40,24 +38,49 @@ def generation():
         for y in range(HAUTEUR // 25):
             etat = rd.randint(0, 2)
             if etat == 0:
-                canvas.create_rectangle((x * 25, y * 25),
-                                        (x * 25 + 25, y * 25 + 25),
-                                        fill="blue")
-                liste_parcelle_eau.append([x, y])
+                case_actuelle = []
+                case_actuelle.append("blue")
+                case_actuelle.append(canvas.create_rectangle((x * 25, y * 25),
+                                                             (x * 25 + 25,
+                                                              y * 25 + 25),
+                                                             fill="blue"))
+                liste_parcelle.append(case_actuelle)
             if etat == 1:
-                canvas.create_rectangle((x * 25, y * 25),
-                                        (x * 25 + 25, y * 25 + 25),
-                                        fill="green")
-                liste_parcelle_foret.append([x, y])
+                case_actuelle = []
+                case_actuelle.append("green")
+                case_actuelle.append(canvas.create_rectangle((x * 25, y * 25),
+                                                             (x * 25 + 25,
+                                                             y * 25 + 25),
+                                                             fill="green"))
+                liste_parcelle.append(case_actuelle)
             if etat == 2:
-                canvas.create_rectangle((x * 25, y * 25),
-                                        (x + 25 * 25, y * 25 + 25),
-                                        fill="yellow")
-                liste_parcelle_plaine.append([x, y])
+                case_actuelle = []
+                case_actuelle.append("yellow")
+                case_actuelle.append(canvas.create_rectangle((x * 25, y * 25),
+                                                             (x + 25 * 25,
+                                                             y * 25 + 25),
+                                                             fill="yellow"))
+                liste_parcelle.append(case_actuelle)
+
+
+def start():
+    """Lance la simulation en affichant le nombre de
+    cases en feu et l'étape de la simulation"""
+    boutton_start.config(text="Pause", command=pause)
+
+
+def pause():
+    """Mets la simulation en pause"""
+    boutton_start.config(text="Start", command=start)
+
+
+def check():
+    """Checking des cases adjacentes"""
 
 
 ###############################
 # Programme principal
+
 
 racine = tk.Tk()
 racine.config(bg="black")
@@ -75,11 +98,14 @@ boutton_sauver = tk.Button(racine, text="Sauvegarder",
 boutton_charger = tk.Button(racine, text="Charger", font=("Helvatica", "20"),
                             bg="black", fg="white")
 
+boutton_start = tk.Button(racine, text="Start", font=("Helvatica", "20"),
+                          bg="black", fg="white", command=start)
+
 
 canvas.grid(column=0, rowspan=3)
 boutton_generation.grid(row=0, column=1)
 boutton_sauver.grid(row=1, column=1)
 boutton_charger.grid(row=2, column=1)
-
+boutton_start.grid(row=3, column=0)
 
 racine.mainloop()
